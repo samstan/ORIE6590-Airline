39af812ce1786230af56bcd6c06d3f635df40898
tsamtsam <tsam@g.hmc.edu>
2021-04-14 14:34:27 -0700

    d

--- airline/envs/airline.py
+++ airline/envs/airline.py
@@ -3,35 +3,20 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
 
-class NServerEnv(gym.Env):
+class AirlineEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, config):
         
         # Initializes model parameters based on a configuration dictionary
-        self.rho = config['rho']
-        self.N = config['N']
-        self.h1 = config['h1']
-        self.h2 = config['h2']
+        self.A = config['A']
+        self.f = config['f']
+        self.P = config['P']
         self.starting_state = config['starting_state']
 
-        # Updates arrival rates
-
-        self.lambda1 = 1.3*self.rho
-        self.lambda2 = 0.4*self.rho
-
-        # Defines service rates
-        self.mu1 = 1
-        self.mu2 = 0.5
-        self.mu3 = 1
-
-
-        # Normalization factor for the uniformization
-        self.B = self.lambda1 + self.lambda2 + self.mu1 + self.mu2 + self.mu3
-
         # Defines state and action spaces, sets current state to be starting_state
-        self.action_space = gym.spaces.MultiDiscrete([2,4])
-        self.observation_space = gym.spaces.MultiDiscrete([self.N+1,self.N+1])
+        self.action_space = gym.spaces.MultiDiscrete([1]*len(self.A.shape[1]))
+        self.observation_space = gym.spaces.MultiDiscrete(self.starting_state)
         self.state = np.asarray(self.starting_state)
 
     # Resets environment to initial state
@@ -40,20 +25,20 @@ class NServerEnv(gym.Env):
         return self.state
 
     # Defines one step of the MDP, returning the new state, reward, whether time horizon is finished, and a dictionary of information
-    def step(self, action):
-        reward = self.r(self.state)
+    def step(self, action, t):
         
         # Just for personal double checking for Q learning algortihm, can ignore this
         # if self.state[0] == self.N and self.state[1] == self.N and action[0] == 0 and action[1] == 0:
             # print('Uh oh, stuck at absorbing state')
         
         
-        trans = self.pr(self.state, action)
+        trans = self.pr(self.state, action, t)
         states = list(trans.keys())
         probs = list(trans.values())
         # Computes new state
-        newState = states[np.random.choice(range(len(states)), 1, p = probs)[0]]
-        self.state = np.asarray(newState)
+        newState = np.asarry(states[np.random.choice(range(len(states)), 1, p = probs)[0]])
+        reward = self.r(self.state, newState)
+        self.state = newState
         episode_over = False
 
         return self.state, reward, episode_over, {}
@@ -61,31 +46,20 @@ class NServerEnv(gym.Env):
 
 
     # Auxilary function computing the reward
-    def r(self, state):
-        return -(self.h1*state[0] + self.h2*state[1])/self.B
+    def r(self, state, newState):
+        if np.all(state == newState):
+            return 0
+        else:
+            difInd = np.amax(state-newState)
+            return f[difInd]
 
     # Auxilary function computing transition distribution
-    def pr(self, state, action):
+    def pr(self, state, action, t):
         transition_probs = {}
-        if state[0] < self.N:
-            transition_probs[(state[0]+1, state[1])] = self.lambda1/self.B
-        
-        if state[1] < self.N:
-            transition_probs[(state[0], state[1]+1)] = self.lambda2/self.B
-
-        if state[0] > 0:
-            if action[0] == 1 and action[1] == 2:
-                transition_probs[(state[0]-1, state[1])] = (self.mu1+self.mu2)/self.B
-            elif action[0] == 1:
-                transition_probs[(state[0]-1, state[1])] = (self.mu1)/self.B
-            elif action[1] == 2:
-                transition_probs[(state[0]-1, state[1])] = (self.mu2)/self.B
-
-                
-        if state[1] > 0:
-            if action[1] == 3:
-                transition_probs[(state[0], state[1]-1)] = (self.mu3)/self.B
-
-        transition_probs[(state[0], state[1])] = 1 - sum(transition_probs.values())
+        for j in range(len(action)):
+            nState = state - np.matmul(self.A[:, j ], action[j])
+            if not np.all(nState == state):
+                transition_probs[nState] == self.P[t, j]
+        transition_probs[state] = 1 - sum(transition_probs.values())
         
         return transition_probs
